a732a747320cc79fe0fb445fbc418c03342897c4
Sundareeshwaran D <96060751+Sundareeshwaran@users.noreply.github.com>
2024-04-21 11:30:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': 'Josefin Sans'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
